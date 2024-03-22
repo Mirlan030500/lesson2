01e6f056ed7a749b86d4719d68f992e9eda4aa68
mirlan030500 <usubalievmirlan.030500@gmail.com>
2024-03-22 18:57:41 +0600

    first commit

---
+++ lesson2_practice.dart
@@ -0,0 +1,65 @@
+void main(){
+num temperature = 25; 
+num distance = 10.5; 
+num count = 1000000;
+num price = 99.99; 
+num result = 399; 
+
+int age = 25;
+int population = 1000000;
+int quantity = 500;
+int score = -10;
+int friuct = 10;
+
+double a = 25.5;
+double b = 10.75;
+double c = 99.99;
+double p = 3.14;
+double e = 1.3;
+
+String name = "Mirlan";
+String message = "Hello, world!";
+String address = "123 Main Street";
+String email = "example@email.com";
+String greeting = "Welcome";
+
+bool isRaining = true;
+bool isLoggedIn = false;
+
+List<int> numbers = [1, 2, 3, 4, 5];
+List<String> fruits = ["apple", "banana", "orange"];
+List<dynamic> listNumbergit  = [1, "two", true];
+List<double> prices = [9.99, 19.99, 4.99];
+List<bool> conditions = [true, false, true];
+
+Map<String, int> ages = {"Mirlan": 25, "Aman": 30, "Asan": 35};
+Map<String, String> countries = {"KG": "Kyrgystan", "UK": "United Kingdom"};
+Map<String, dynamic> userDetails = {"name": "Mirlan", "age": 25, "isStudent": true};
+Map<int, String> indexToName = {0: "Alina", 1: "Malina", 2: "Malena"};
+Map<String, bool> permissions = {"read": true, "write": false};
+
+final int maxAttempts = 3;
+final String appName = "lessonApp";
+final double taxRate = 0.15;
+final bool isDebugMode = false;
+final List<String> colors = ["red", "green", "blue"];
+
+const int maxScore = 100;
+const double valuePi = 3.14;
+const String fileName = "lessonApp";
+const List<int> number = [1, 2, 3, 4, 5];
+const Map<String, int> info = {"Dastan": 25, "Asan": 30, "Aman": 35};
+
+dynamic value = 10;
+value = "hello";
+value = true;
+List<dynamic> mixList = [1, "two", true];
+Map<dynamic, dynamic> dynamicMap = {"key": 10, "value": "hello"};
+
+var firstname = "Mirlan";
+var twentyFive = 25;
+var twentyFivepointFive = 25.5;
+var untrue = false;
+var fiveNumber = [1, 2, 3, 4, 5];
+
+}
